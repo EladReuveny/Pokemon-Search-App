be39cbda2417648e20a889f8b57125f0a0192b6f
EladReuveny <126913072+EladReuveny@users.noreply.github.com>
2024-10-23 00:37:54 +0300

    3rd commit - Adding evolutions feature

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -5,16 +5,76 @@ import pokemonIcon from "../assets/pokemon-icon.svg";
 const InputForm = ({ setPokemon }) => {
   const [searchValue, setSearchValue] = useState("");
 
-  const searchPokemon = () => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${searchValue}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
+  const searchPokemon = async () => {
+    try {
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${searchValue.toLowerCase()}`
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      setPokemon(data);
+
+      const dataSpeciesUrl = data.species.url;
+
+      const speciesResponse = await fetch(dataSpeciesUrl);
+      if (!speciesResponse.ok) {
+        throw new Error(`HTTP error! status: ${speciesResponse.status}`);
+      }
+      const speciesData = await speciesResponse.json();
+
+      const evolutionChainUrl = speciesData.evolution_chain.url;
+
+      const evolutionChainResponse = await fetch(evolutionChainUrl);
+      if (!evolutionChainResponse.ok) {
+        throw new Error(`HTTP error! status: ${evolutionChainResponse.status}`);
+      }
+      const evolutionChainData = await evolutionChainResponse.json();
+
+      const secondLvl = evolutionChainData.chain.evolves_to[0]?.species.name;
+      const thirdLvl =
+        evolutionChainData.chain.evolves_to[0]?.evolves_to[0]?.species.name;
+
+      const response2 = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${secondLvl}`
+      );
+      if (!response2.ok) {
+        throw new Error(`HTTP error! status: ${response2.status}`);
+      }
+      const data2 = await response2.json();
+
+      const secondLevelData = {
+        name: data2.name,
+        img: data2.sprites.front_default,
+      };
+
+      let thirdLevelData = null;
+
+      if (thirdLvl) {
+        const response3 = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${thirdLvl}`
+        );
+        if (!response3.ok) {
+          throw new Error(`HTTP error! status: ${response3.status}`);
         }
-        return response.json();
-      })
-      .then((data) => setPokemon(data))
-      .catch((error) => alert("Error: Invalid input! Try again."));
+        const data3 = await response3.json();
+
+        thirdLevelData = {
+          name: data3.name,
+          img: data3.sprites.front_default,
+        };
+      }
+
+      setPokemon((p) => ({
+        ...p,
+        secondLvl: secondLevelData,
+        thirdLvl: thirdLevelData,
+      }));
+    } catch (error) {
+      alert("Error: Invalid input! Try again.");
+      console.error(error);
+    }
   };
 
   return (
